eabbdd4694ba416e8d9bb0c1c3df7700f34a2443
enosintech <enos.nsambaj@gmail.com>
2024-05-12 15:24:12 +0200

    test

--- src/components/LandingBg.jsx
+++ src/components/LandingBg.jsx
@@ -1,19 +1,12 @@
-import { useGSAP } from "@gsap/react";
-
 import ModelView from "./ModelView";
 import HeroInfo from "./HeroInfo";
 
 import { landingBgVideo } from "../lib";
-import { endTriggerPinAnimations } from "../utils/animations";
 
 const LandingBg = () => {
 
-    useGSAP(() => {
-        endTriggerPinAnimations("#bgPin");
-    }, [])
-
   return (
-    <div id="bgPin" className="w-full h-[100vh] bg-neutral-800 flex items-center justify-center absolute z-[-1]">
+    <div className="w-full h-[100vh] bg-neutral-800 flex items-center justify-center fixed z-[-1]">
         <div className="absolute w-full h-full top-0 left-0 z-[-1]">
         <video key={landingBgVideo} className="w-full h-full object-cover" playsInline={true} loop={true} muted={true} autoPlay={true} controls={false}>
             <source src={landingBgVideo} type="video/mp4" />
